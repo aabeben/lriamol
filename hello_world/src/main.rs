bb12808b8f9b8d67c0aac3f31d6147b67247b752
aabeben <bhasanudin@gmail.com>
2024-10-26 09:31:51 +0700

    Still talking about comments.

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    // Rust program starts with fn main()
-    // you put the code inside a block.
-    // It starts with { and ends with }
-    let _some_number = 100; // We can write as much as we want here and the compiler won't look at it.
-    let another_number/* :i16 */=100;
+    let some_number = 100; // let me tell you
+                           // about this number
+                           // It's 100, this is my favorite number
+    let some_number = 100;
+    // It's called some_number but actually I think that...    let some_number = 100; /* Let me tell you    a little about this number.    It's 100, which is my favorite number.    It's called some_number but actually I think that... */
 }
