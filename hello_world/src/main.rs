8d23ed4ced85df345bacb5f2769924c216f63f80
aabeben <bhasanudin@gmail.com>
2024-10-26 09:14:25 +0700

    Using /**/ for commenting.

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,5 +1,7 @@
 fn main() {
-    let mut name: String = "Dave".to_string();
-    println!("Hello, {}!", name.to_string());
-    name.push_str("!");
+    // Rust program starts with fn main()
+    // you put the code inside a block.
+    // It starts with { and ends with }
+    let _some_number = 100; // We can write as much as we want here and the compiler won't look at it.
+    let another_number/* :i16 */=100;
 }
