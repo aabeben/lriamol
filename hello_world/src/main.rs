928a08cbc57cc3f8bba16e06987a0af0ad3fbd06
aabeben <bhasanudin@gmail.com>
2024-10-27 11:17:10 +0700

    casting in rust.

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,9 +1,4 @@
 fn main() {
-    /// this comment will show if you
-    /// run the command cargo doc --open
-    let some_number = 100; // let me tell you
-                           // about this number
-                           // It's 100, this is my favorite number
-    let some_number = 100;
-    // It's called some_number but actually I think that...    let some_number = 100; /* Let me tell you    a little about this number.    It's 100, which is my favorite number.    It's called some_number but actually I think that... */
+    let num: u8 = 100;
+    println!("{}", num as char);
 }
